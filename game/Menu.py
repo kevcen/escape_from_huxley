c6aff3e2c9bb10ce075dde9af19652ea50c73edd
Izer <ieo119@ic.ac.uk>
2020-02-09 06:37:15 +0000

    options

--- game/Menu.py
+++ game/Menu.py
@@ -13,18 +13,11 @@ def Menu(display, pg):
     background = pg.transform.scale(background, (display.get_width(), display.get_height()))
     title_font = pg.font.SysFont("papyrus", 120)
     title_text = title_font.render("ESCAPE FROM HUXLEY", 1, col.RED.value)
-    enter_font = pg.font.SysFont("papyrus", 80)
-    enter_text = enter_font.render("Press enter to play", 1, col.BLUE.value)
     display.blit(background, (0, 0))
     display.blit(title_text, ((display.get_width() -
                                title_text.get_width()) // 2,
                               (display.get_height() - title_text.get_height())
                               // 2 - 100))
-
-    display.blit(enter_text, ((display.get_width() -
-                               enter_text.get_width()) // 2,
-                              (display.get_height() - enter_text.get_height())
-                              // 2))
 
     while not play:
 
